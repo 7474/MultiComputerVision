37ec856591924dab193a8109b759ef1cc1f1a56a
koudenpa <koudenpa@hotmail.com>
2020-05-08 00:04:48 +0900

    マイグレーションスクリプト生成用のお試し。
    https://docs.microsoft.com/ja-jp/ef/core/miscellaneous/cli/dbcontext-creation

---
+++ BlazorMultiComputerVisionWebasm/Server/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMultiComputerVisionWebasm.Server.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            // XXX local db 効くのだろうか？
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-BlazorMultiComputerVisionWebasm.Server-E0E5C506-D56E-4944-91E0-9F4C64AB4E4B;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new ApplicationDbContext(optionsBuilder.Options, null);
+        }
+    }
+}
