28ca718213777feebd55a9bb69f14f8c416765ac
koudenpa <koudenpa@hotmail.com>
2020-05-08 21:45:19 +0900

    認証用DBの定義をライブラリに切り出し。

--- MultiComputerVisionIdentityService/Data/DesignTimeDbContextFactory.cs
+++ MultiComputerVisionIdentityService/Data/DesignTimeDbContextFactory.cs
@@ -13,7 +13,6 @@ namespace BlazorMultiComputerVisionWebasm.Server.Data
     {
         public ApplicationDbContext CreateDbContext(string[] args)
         {
-            // XXX local db 効くのだろうか？
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
             optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-BlazorMultiComputerVisionWebasm.Server-E0E5C506-D56E-4944-91E0-9F4C64AB4E4B;Trusted_Connection=True;MultipleActiveResultSets=true");
 
